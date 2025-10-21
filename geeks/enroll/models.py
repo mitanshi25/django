fe145e65af6c96ee4adf99381d6b1c1f87efcd56
mitanshi <mitanshi25ksj@gmail.com>
2025-10-21 23:10:00 +0530

    stash

--- geeks/enroll/models.py
+++ geeks/enroll/models.py
@@ -6,3 +6,13 @@ class Student(models.Model):
     st_l_name= models.CharField(max_length=70)
     created_at = models.DateTimeField()
 # Create your models here.
+
+# will create a new model 
+
+# will add another line of code here
+
+# hi i am master
+
+# hi i am feature 
+
+# demo 
